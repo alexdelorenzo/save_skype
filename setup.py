2781649b5937f6485b0ac10ddb4acaf2fc745287
alex <alex>
2016-10-16 22:43:29 -0400

    Update version for PyPI

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('requirements.txt', 'r') as file:
     requirements = file.readlines()
 
 setup(name="save_skype",
-      version="0.1.1",
+      version="0.1.4",
       description="Extract and save Skype chats.",
       url="https://github.com/thismachinechills/save_skype",
       author="thismachinechills (Alex)",
@@ -14,4 +14,4 @@ setup(name="save_skype",
       install_requires=requirements,
       keywords="skype main.db extract chats".split(' '),
       entry_points={"console_scripts":
-                        ["save_skype = save_skype.extract:cmd"]})
+                        ["save_skype = save_skype.extract:chats_to_files"]})
