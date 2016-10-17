ce73590211ce31a2a26255b7e4ea873c7b8cd843
alex <alex>
2016-10-16 22:30:05 -0400

    Use requirement.txt entries to populate package requirements

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
 from setuptools import setup
 
+with open('requirements.txt', 'r') as file:
+    requirements = file.readlines()
 
 setup(name="save_skype",
-      version="0.1",
+      version="0.1.1",
       description="Extract and save Skype chats.",
       url="https://github.com/thismachinechills/save_skype",
       author="thismachinechills (Alex)",
       license="AGPL 3.0",
       packages=['save_skype'],
       zip_safe=True,
-      install_requires=["click", "html_wrapper"],
+      install_requires=requirements,
       keywords="skype main.db extract chats".split(' '),
       entry_points={"console_scripts":
                         ["save_skype = save_skype.extract:cmd"]})
